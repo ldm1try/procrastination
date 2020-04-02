6bd0519fc06c4b4833abe419c7c389e133506f52
Dmitry <dimm4eg@gmail.com>
2020-04-02 22:31:50 +1000

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,8 +23,8 @@ let phrases = [
   
   button.addEventListener('click', function () {
     let randomElement = getRandomElement(phrases);
-    smoothly(phrase, 'textContent', randomElement.text)
-    smoothly(image, 'src', randomElement.image)
+    smoothly(phrase, 'textContent', randomElement.text);
+    smoothly(image, 'src', randomElement.image);
 
     if (randomElement.text.length > 40) {
         advice.style.fontSize = '33px';
